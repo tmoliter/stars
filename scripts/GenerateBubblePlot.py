f9f212e37705de5faf433bab5b81d308ddc5ab90
Tim Moliterno <tim@admithub.com>
2021-01-04 13:02:08 -0500

    find lunar phases and cordon off deprecated crap (#3)

--- scripts/GenerateBubblePlot.py
+++ scripts/GenerateBubblePlot.py
@@ -1,16 +1,16 @@
 from datetime import datetime
-from typing import List
 from utils import Exobody, get_planetary_plot_data, yearly_date_dict_to_flat_datetimes
-from skyfield.magnitudelib import planetary_magnitude
 import matplotlib.pyplot as plt
-from data.dates import hardcoded_lunar_dates
+from dateutil.tz import *
 
 fig, ax = plt.subplots()
 
 
-def plot_all_bodies(dates: List[datetime]):
+def plot_all_bodies(start: datetime, end: datetime):
 
-    planetary_plotting_data = get_planetary_plot_data(dates, [body for body in Exobody])
+    planetary_plotting_data = get_planetary_plot_data(
+        start, end, [body for body in Exobody]
+    )
 
     for body, date in planetary_plotting_data.items():
         for plot_datum in date:
@@ -22,8 +22,9 @@ def plot_all_bodies(dates: List[datetime]):
 
 if __name__ == "__main__":
 
-    dates = yearly_date_dict_to_flat_datetimes(2021, hardcoded_lunar_dates[2021])
-    plot_all_bodies(dates)
+    start = datetime(2020, 12, 1, tzinfo=tzutc())
+    end = datetime(2020, 12, 31, tzinfo=tzutc())
+    plot_all_bodies(start, end)
 
     plt.xlim([0, 360])
     plt.ylim([-65, 65])
