7b4e290af37b7c1bd921203a47890fe4afab314a
Timothy Moliterno <tim@admithub.com>
2021-01-10 15:01:26 -0500

    map on scale

--- frontend/test.js
+++ frontend/test.js
@@ -10,7 +10,7 @@ d3.select("#submit").on("click", () => {
       .get(`http://bugenhagen.herokuapp.com/?start=${start}&end=${end}`)
       .then((res) => {
         // d3.select("#graph").text(JSON.stringify(res.data))
-        makeGraph(res.data)
+        makeGraph(res.data);
       });
   }
 });
@@ -19,7 +19,6 @@ const makeGraph = (data) => {
   const margin = { top: 20, bottom: 40, left: 30, right: 20 };
   const width = 800 - margin.left - margin.right;
   const height = 600 - margin.top - margin.bottom;
-  console.log(data)
 
   // Creates sources <svg> element
   const svg = d3
@@ -29,56 +28,46 @@ const makeGraph = (data) => {
     .attr("height", height + margin.top + margin.bottom);
 
   // Group used to enforce margin
-  const g = svg
+  const topLevelG = svg
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  const xscale = d3
-    .scaleLinear()
-    .domain([0, 360])
-    .range([0, width]);
-  const yscale = d3
-    .scaleLinear()
-    .domain([-65, 65])
-    .range([height, 0]);
+  const xscale = d3.scaleLinear().domain([0, 360]).range([0, width]);
+  const yscale = d3.scaleLinear().domain([-65, 65]).range([height, 0]);
 
+  // BEGIN AXIS LABEL CODE
   const xaxis = d3.axisBottom(xscale);
   const yaxis = d3.axisLeft(yscale);
 
-  // console.log("1")
-  // g.append("g")
-  //   .classed("x.axis", true)
-  //   .attr("transform", `translate(0,${height})`)
-  //   .call(xaxis);
-  // g.append("g").classed("y.axis", true).call(yaxis);
-  const group = g.append("g");
+  console.log("1");
+  topLevelG
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(xaxis);
+  topLevelG.append("g").call(yaxis);
+  // END AXIS LABEL CODE
 
-  console.log("2")
+  const group = topLevelG.append("g");
 
-  const flatData = Object.values(data).flat()
-  console.log(flatData)
+  const flatData = Object.values(data).flat();
 
   const marks = group
     .selectAll("circle")
     .data(flatData)
     .join(
       (enter) => {
-        const marks_enter = enter.append("circle");
-        marks_enter.attr("r", 10).append("title");
+        const marks_enter = enter.append("circle").attr("r", 10);
+        marks_enter.append("title");
         return marks_enter;
       },
       (update) => update,
       (exit) => exit.remove()
     );
 
-  console.log("3")
   marks
     .style("fill", (planet) => planet.color)
-    .attr("cx", (planet) => planet.x)
-    .attr("cy", (planet) => planet.y);
+    .attr("cx", (planet) => xscale(planet.x))
+    .attr("cy", (planet) => yscale(planet.y));
 
-  console.log(marks)
   marks.select("title").text((d) => d.name);
-
-
 };
