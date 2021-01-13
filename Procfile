9bbecf47268e512146a7d7d00fd1eaca5c978de7
Timothy Moliterno <tim@admithub.com>
2021-01-13 08:29:26 -0500

    attempt to load data when app loads

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: cd backend && python3 server.py
+worker: cd backend && python3 data.py
--- backend/data.py
+++ backend/data.py
@@ -1,3 +1,8 @@
 from skyfield.api import load
 
 eph = load("de421.bsp")
+
+
+if __name__ == "__main__":
+    # Run this at start-up to prevent long response time on first request
+    eph
